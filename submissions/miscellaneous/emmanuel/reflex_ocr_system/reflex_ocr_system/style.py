48d4754e3a05ebb7b054d41f61592ccd1986378e
emmakodes <emmamichael65@gmail.com>
2023-10-31 20:02:33 +0000

    reflex ocr system

---
+++ submissions/miscellaneous/emmanuel/reflex_ocr_system/reflex_ocr_system/style.py
@@ -0,0 +1,24 @@
+style = {
+    "background-color": "#454545",
+    "font_family": "Comic Sans MS",
+    "font_size": "16px",
+}
+
+topic_style = {
+    "color": "white",
+    "font_family": "Comic Sans MS",
+    "font_size": "3em",
+    "font_weight": "bold",
+    "box_shadow": "rgba(190, 236, 0, 0.4) 5px 5px, rgba(190, 236, 0, 0.3) 10px 10px",
+    "margin-bottom": "3rem",
+}
+
+
+extracted_text_style = {
+    "color": "white",
+    "text-align": "center",
+    "font_size": "0.9rem",
+    "width": "80%",
+    "display": "inline-block",
+    "display": "inline-block",
+}
